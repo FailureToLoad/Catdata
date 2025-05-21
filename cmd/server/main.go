d30fc78e25786ff22912cb5738b00ac7fb119d49
D.Mason <failuretoload@outlook.com>
2025-05-20 23:28:50 -0700

    Adds stub for main executable

---
+++ cmd/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/failuretoload/catdata/routes/index"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	index.RegisterRoutes(r)
+
+	slog.Info("server started on :8080")
+	http.ListenAndServe(":8080", r)
+}
