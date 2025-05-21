2ccc8e7005d144e3dac6a9690fc989f9ce7f9a68
D.Mason <failuretoload@outlook.com>
2025-05-20 23:18:58 -0700

    Adds Cats func to service

--- cat/service.go
+++ cat/service.go
@@ -2,10 +2,12 @@ package cat
 
 import (
 	"context"
+	"errors"
 
 	"github.com/failuretoload/catdata/cat/domain"
 )
 
+//go:generate mockgen -source=service.go -destination=mock/mock_service.go -package=mock
 type (
 	Repo interface {
 		Query(ctx context.Context, input domain.QueryInput) ([]domain.CatRecord, error)
@@ -15,8 +17,29 @@ type (
 	}
 )
 
+var (
+	ErrOffset = errors.New("offset cannot be negative")
+	ErrLimit  = errors.New("limit cannot be negative")
+)
+
 func NewService(r Repo) Service {
 	return Service{
 		repo: r,
 	}
+}
+
+func (s Service) Cats(ctx context.Context, offset, limit int) ([]domain.CatRecord, error) {
+	if offset < 0 {
+		return nil, ErrOffset
+	}
+	if limit < 0 {
+		return nil, ErrLimit
+	}
+
+	input := domain.QueryInput{
+		Offset: offset,
+		Limit:  limit,
+	}
+
+	return s.repo.Query(ctx, input)
 }
---
+++ cat/service_test.go
@@ -0,0 +1,103 @@
+package cat_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/failuretoload/catdata/cat"
+	"github.com/failuretoload/catdata/cat/domain"
+	"github.com/failuretoload/catdata/cat/mock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func TestService_Cats(t *testing.T) {
+	t.Run("limit can't be negative", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		repoFake := mock.NewMockRepo(ctrl)
+		target := cat.NewService(repoFake)
+		records, err := target.Cats(context.Background(), 0, -1)
+		require.ErrorIs(t, err, cat.ErrLimit)
+		assert.Empty(t, records)
+	})
+
+	t.Run("offset can't be negative", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		repoFake := mock.NewMockRepo(ctrl)
+		target := cat.NewService(repoFake)
+		records, err := target.Cats(context.Background(), -1, 0)
+		require.ErrorIs(t, err, cat.ErrOffset)
+		assert.Empty(t, records)
+	})
+
+	t.Run("reports repo errors", func(t *testing.T) {
+		limit := 1
+		offset := 1
+		ctrl := gomock.NewController(t)
+		repoFake := mock.NewMockRepo(ctrl)
+		repoFake.EXPECT().
+			Query(gomock.Any(), domain.QueryInput{
+				Limit:  limit,
+				Offset: offset,
+			}).
+			Return(
+				nil,
+				errors.New("repo error"),
+			)
+
+		target := cat.NewService(repoFake)
+		records, err := target.Cats(context.Background(), offset, limit)
+		require.Error(t, err)
+		assert.ErrorContains(t, err, "repo error")
+		assert.Empty(t, records)
+	})
+
+	t.Run("return records on success", func(t *testing.T) {
+		limit := 3
+		offset := 0
+		ctrl := gomock.NewController(t)
+		repoFake := mock.NewMockRepo(ctrl)
+		nimbusNote := "acquiring mass"
+		yetiNote := "wants an egg"
+		romNote := "stop eating carpet"
+		expected := []domain.CatRecord{
+			{
+				Cat:       "nimbus",
+				Weight:    13,
+				Timestamp: time.Now().Unix(),
+				Notes:     &nimbusNote,
+			},
+			{
+				Cat:       "yeti",
+				Weight:    10,
+				Timestamp: time.Now().Unix(),
+				Notes:     &yetiNote,
+			},
+			{
+				Cat:       "rom",
+				Weight:    11,
+				Timestamp: time.Now().Unix(),
+				Notes:     &romNote,
+			},
+		}
+		repoFake.EXPECT().
+			Query(gomock.Any(), domain.QueryInput{
+				Limit:  limit,
+				Offset: offset,
+			}).
+			Return(
+				expected,
+				nil,
+			)
+
+		target := cat.NewService(repoFake)
+		records, err := target.Cats(context.Background(), offset, limit)
+		require.NoError(t, err)
+		assert.NotEmpty(t, records)
+		assert.ElementsMatch(t, expected, records)
+	})
+
+}
