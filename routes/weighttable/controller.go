2d69ac1660a5a562383d255a2bd7af721b659c57
D.Mason <failuretoload@outlook.com>
2025-05-20 23:32:26 -0700

    fleshes out fetchRows

--- routes/weighttable/controller.go
+++ routes/weighttable/controller.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 
 	"github.com/failuretoload/catdata/cat"
+	"github.com/failuretoload/catdata/response"
 	"github.com/go-chi/chi/v5"
+	datastar "github.com/starfederation/datastar/sdk/go"
 )
 
 type Controller struct {
@@ -22,5 +24,15 @@ func (c Controller) RegisterRoutes(r *chi.Mux) {
 }
 
 func (c Controller) fetchRows(w http.ResponseWriter, r *http.Request) {
-	panic("implement me")
+	ctx := r.Context()
+	limit := 50
+	offset := 0
+
+	rows, err := c.service.Cats(ctx, offset, limit)
+	if err != nil {
+		response.InternalServerError(ctx, w, "unable to request records", err)
+	}
+
+	sse := datastar.NewSSE(w, r)
+	sse.MergeFragmentTempl(Rows(rows))
 }
